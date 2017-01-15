d3ca8f746a6eb92f2de7959940184e0e667558e9
lishuo <lishuo@moxiu.net>
2017-01-15 16:28:20 +0800

    倒计日调整

--- dateCount.php
+++ dateCount.php
@@ -8,6 +8,13 @@ namespace Count;
  */
 class dateCount
 {
+    /**
+     * getParam
+     *
+     * get params from console
+     * @access private
+     * @return Array
+     */
     private function getParam()
     {
         $param = getopt('s:e:');
@@ -19,11 +26,22 @@ class dateCount
         return $param;
     }
 
+    /**
+     * getCount
+     *
+     * calculate days between stiem and etime
+     * @access public
+     * @return void
+     */
     public function getCount()
     {
         $param = $this->getParam();
         $end = strtotime($param['end']);
         $start = strtotime($param['start']);
+
+        $second = (int) $end - time();
+        $second = number_format($second);
+        echo "距当前还有$second 秒\n";
 
         $count = ($end - $start) / 86400;
         $count = (int) $count;
@@ -31,6 +49,14 @@ class dateCount
         $this->formatDate($param);
     }
 
+    /**
+     * formatDate
+     *
+     * output to console
+     * @param mixed $param
+     * @access private
+     * @return void
+     */
     private function formatDate($param)
     {
         $count = $param['count'];
@@ -47,6 +73,14 @@ class dateCount
         }
     }
 
+    /**
+     * formatYear
+     *
+     * format time to Chinese
+     * @param mixed $days
+     * @access private
+     * @return String
+     */
     private function formatYear($days)
     {
         $days = abs($days);
@@ -57,11 +91,11 @@ class dateCount
         if ($days > 365) {
             $years = (int) ($days / 365);
             $days = $days % 365;
-        } 
+        }
         if ($days > 31) {
             $month = (int) ($days / 31);
             $days = $days % 31;
-        } 
+        }
         $res = '';
         $years && $res .= $years . '年';
         $month && $res .= $month . '个月';
@@ -72,6 +106,4 @@ class dateCount
 
 $DateCount = new dateCount();
 $count = $DateCount->getCount();
-
-
 
